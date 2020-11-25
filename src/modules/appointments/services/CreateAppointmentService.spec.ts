e8d7f075463dba67b11bd579d0e5317c1e17cee7
Brunokkkk <bruno.coelho.santos17@gmail.com>
2020-11-24 21:50:16 -0300

    terminei os testes com Appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,7 @@
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from './CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
+
 import {expect, describe, it} from '@jest/globals';
 
 describe('CreateAppoitment', () => {
@@ -16,5 +18,24 @@ describe('CreateAppoitment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('111');
+  })
+
+  it('should be able to create a new appointment', async() => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointmentDate = new Date(2020, 4, 10);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '111'
+    });
+
+    expect(createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '111'
+    })).rejects.toBeInstanceOf(AppError);
   })
 })
