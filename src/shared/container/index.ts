8f5c89babc87ece807b7a43b3c37a93315415db7
Brunokkkk <bruno.coelho.santos17@gmail.com>
2021-02-16 19:50:23 -0300

    controle de perfil

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,6 @@ import './providers';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentRepository';
-
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUsersTokenRepository from '@modules/users/repositories/IUserTokenRepository';
