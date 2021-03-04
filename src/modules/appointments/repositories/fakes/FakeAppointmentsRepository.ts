87ef780f6e521aaf6d95bdcfd8964444c769854d
Brunokkkk <bruno.coelho.santos17@gmail.com>
2021-03-03 22:28:16 -0300

    listar datas

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,21 +1,36 @@
-import {uuid} from 'uuidv4';
-import {isEqual} from 'date-fns';
+import { uuid } from 'uuidv4';
+import { isEqual, getMonth, getYear } from 'date-fns';
 
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 import Appointment from '../../infra/typeorm/entities/Appointments';
 
 class AppointmentRepository implements IAppointmentRepository {
-
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppoitment = this.appointments.find(
-      appointment => isEqual(appointment.date, date),
+    const findAppoitment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppoitment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      appointment =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year,
+    );
+
+    return appointments;
   }
 
   public async create({
@@ -24,12 +39,11 @@ class AppointmentRepository implements IAppointmentRepository {
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appoitment = new Appointment();
 
-    Object.assign(appoitment, {id: uuid(), date, provider_id});
+    Object.assign(appoitment, { id: uuid(), date, provider_id });
 
     this.appointments.push(appoitment);
 
     return appoitment;
-
   }
 }
 
